5bd41718a82eba900fe24cea378aa3eafab8e2f4
Zavians <kenzatekkom20@gmail.com>
2023-09-24 21:30:14 +0700

    modified:   src/App.jsx 	modified:   src/elements/Button.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
+import Button from "./elements/button";
+import TextInput from "./elements/TextInput";
 
 const peopleBio = [
   {
     name: "Kenza Vianda Dwiputra",
     age: 21,
     text: "Semoga Saya Gk NT",
-    profile: "https://avatars.githubusercontent.com/u/81897441?v=4",
+    profile: "https://avatars.githubusercontent.com/u/94980985?v=4",
     nim: "21120120140046",
     email: "kenzatekkom20@gmail.com",
     link: "https://github.com/Zavians",
@@ -26,7 +28,7 @@ const peopleBio = [
   {
     name: "Yahya Oktariansyah",
     age: 21,
-    text: "Semoga Kahim Saya Bangga",
+    text: "Saya Kahim Saya Bangga",
     profile: "https://avatars.githubusercontent.com/u/82740376?v=4",
     nim: "21120120140037",
     email: "myoktariansyah@students.undip.ac.id",
@@ -44,6 +46,20 @@ const peopleBio = [
 ];
 
 function App() {
+  const [inputName, setInputName] = useState(''); // State untuk input nama
+  const [filteredPeople, setFilteredPeople] = useState(peopleBio);
+
+  const handleNameInputChange = (event) => {
+    setInputName(event.target.value);
+  };
+
+  const handleFilterPeople = () => {
+    // Filter data orang berdasarkan input nama
+    const filtered = peopleBio.filter((person) =>
+      person.name.toLowerCase().includes(inputName.toLowerCase())
+    );
+    setFilteredPeople(filtered);
+  };
 
   return (
     <>
@@ -55,18 +71,28 @@ function App() {
         <h1>Tugas Praktikum</h1>
         <h2>Ini adalah hasil dari penugasan yang dikerjakan oleh kelompok 21</h2>
       </div>
-      <div className='d-flex flex-wrap justify-content-around w-100 p-3"'>
-        {peopleBio.map((person, index) => (
+      <div>
+          <input
+            type='text'
+            placeholder='Masukkan nama...'
+            value={inputName}
+            onChange={handleNameInputChange}
+            className='TextInput'
+          />
+          <button onClick={handleFilterPeople} className='FilterButton'>Filter</button>
+        </div>
+      <h2>Nama yang dimasukkan: {inputName}</h2>
+      <div className='d-flex flex-wrap justify-content-around w-100 p-3'>
+        
+        {filteredPeople.map((person, index) => (
           <Home key={index} props={person} />
         ))}
       </div>
     </>
-
   );
 }
 
 export default App;
-
 
 
 
