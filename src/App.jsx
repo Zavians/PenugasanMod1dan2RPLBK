bd320db935ee579044b4428ee2c4f3f99ab8925f
Zavians <kenzatekkom20@gmail.com>
2023-09-24 21:28:52 +0700

    modified:   index.html 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.jsx 	new file:   src/components/CompAndProps.jsx 	new file:   src/components/ReactJSX.jsx 	new file:   src/components/header/Header.jsx 	new file:   src/components/header/header.css 	new file:   src/components/home/Home.jsx 	new file:   src/components/home/home.css 	new file:   src/elements/Button.jsx 	new file:   src/elements/TextInput.jsx 	modified:   src/index.css 	modified:   src/App.jsx 	modified:   src/elements/Button.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,72 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+
+const peopleBio = [
+  {
+    name: "Kenza Vianda Dwiputra",
+    age: 21,
+    text: "Semoga Saya Gk NT",
+    profile: "https://avatars.githubusercontent.com/u/81897441?v=4",
+    nim: "21120120140046",
+    email: "kenzatekkom20@gmail.com",
+    link: "https://github.com/Zavians",
+  },
+  {
+    name: "Muhammad Fachrurazi",
+    age: 21,
+    text: "Gk tau gk ada quotes",
+    profile: "https://avatars.githubusercontent.com/u/82750808?v=4",
+    nim: "21120120140104",
+    email: "fachrurazi021@gmail.com",
+    link: "https://github.com/fachru24",
+  },
+  {
+    name: "Yahya Oktariansyah",
+    age: 21,
+    text: "Semoga Kahim Saya Bangga",
+    profile: "https://avatars.githubusercontent.com/u/82740376?v=4",
+    nim: "21120120140037",
+    email: "myoktariansyah@students.undip.ac.id",
+    link: "https://github.com/myoktariansyah",
+  },
+  {
+    name: "Novita Aulya",
+    age: 21,
+    text: "Gaktau Males Mau Beli Truk",
+    profile: "https://avatars.githubusercontent.com/u/82640637?v=4",
+    nim: "21120120140114",
+    email: "novitaaly4@gmail.com",
+    link: "https://github.com/novitaaly",
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='col-md-12'>
+        <Header />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div style={{ textAlign: 'center' }}>
+        <h1>Tugas Praktikum</h1>
+        <h2>Ini adalah hasil dari penugasan yang dikerjakan oleh kelompok 21</h2>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className='d-flex flex-wrap justify-content-around w-100 p-3"'>
+        {peopleBio.map((person, index) => (
+          <Home key={index} props={person} />
+        ))}
+      </div>
     </>
-  )
+
+  );
 }
 
-export default App
+export default App;
+
+
+
+
